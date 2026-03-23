e73f1b4259405bb1fe85cb8114accc349c65924b
codermoksedul <hello@moksedul.dev>
2026-03-23 17:40:52 +0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <div className="w-full max-w-[768px]">
-        <iframe src="https://player.cdn.visualcenter.live/645eee64-2bca-411b-b48d-0ce1b5cdbbe8" style={{ width: '100%', aspectRatio: '16/9', border: 0 }} allow="accelerometer; autoplay; encrypted-media; gyroscope"></iframe>
+        <iframe src="https://player.cdn.moksedul.dev/ff52ce2c-c16c-4de0-a258-3b169d680db5" style={{ width: '100%', aspectRatio: '16/9', border: 0 }} allow="accelerometer; autoplay; encrypted-media; gyroscope"></iframe>
       </div>
     </div>
   )
