9f1e758b21ac6da8e088613153bb8e148ba1ae37
codermoksedul <hello@moksedul.dev>
2026-03-23 17:15:11 +0600

    add

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-[768px]">
+        <iframe src="https://player.cdn.visualcenter.live/645eee64-2bca-411b-b48d-0ce1b5cdbbe8" style={{ width: '100%', aspectRatio: '16/9', border: 0 }} allow="accelerometer; autoplay; encrypted-media; gyroscope"></iframe>
+      </div>
+    </div>
+  )
+}
