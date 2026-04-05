babf52da51ec1d9d9f9c8988412ce5810673c329
Moksedul Islam <123837550+hellomoksedul@users.noreply.github.com>
2026-04-05 10:20:59 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <div className="w-full max-w-[768px]">
-        <iframe src="https://player.cdn.moksdul.dev/30073285-6e2c-461b-a39c-3916e5cafefb" style="width: 100%; aspect-ratio: 16/9; border: 0;" allow="accelerometer; autoplay; encrypted-media; gyroscope" allowfullscreen></iframe>
+        <iframe
+          src="https://player.cdn.moksdul.dev/30073285-6e2c-461b-a39c-3916e5cafefb"
+          style={{ width: "100%", aspectRatio: "16/9", border: "0" }}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
-  )
+  );
 }
